4592ed436b8b846706389fe9cebcf45e85723841
wilder <be.niveau@hotmail.fr>
2019-01-21 17:06:34 +0100

    wilder_adding_button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,19 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <button type="button" class="btn btn-outline-primary mt-3">Inscrire un nouveau wilder</button>
         </div>
     </div>
 </div>
