a8e40f5d7ed7d8d1ed86d980ed8da2b7ecaa2fd6
wilder <be.niveau@hotmail.fr>
2019-01-22 09:38:54 +0100

    registration_link_on_dropdown_menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,7 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
+                                <a class="dropdown-item" href="{{route('wilder')}}">Inscrire un nouveau wilder</a>
                             </div>
                         </li>
                         @endguest
